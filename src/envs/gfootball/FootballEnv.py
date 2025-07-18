3d2f65aa79151a1c9963303068a120989f95cf9e
hello <1277516372@qq.com>
2025-07-18 15:34:34 +0800

    修复bug

--- src/envs/gfootball/FootballEnv.py
+++ src/envs/gfootball/FootballEnv.py
@@ -4,6 +4,7 @@ from gfootball.env import observation_preprocessing
 from ..multiagentenv import MultiAgentEnv
 import gym
 import torch as th
+import numpy as np
 
 
 class GoogleFootballEnv(MultiAgentEnv):
@@ -26,7 +27,13 @@ class GoogleFootballEnv(MultiAgentEnv):
         write_video=False,
         number_of_right_players_agent_controls=0,
         seed=0,
+        sight_field=0.5,
+        state_timestep_number = False
     ):
+        #新加的改动，为了适应kalei
+        self.state_timestep_number = state_timestep_number
+
+        #原先的
         self.dense_reward = dense_reward
         self.write_full_episode_dumps = write_full_episode_dumps
         self.write_goal_dumps = write_goal_dumps
@@ -43,6 +50,7 @@ class GoogleFootballEnv(MultiAgentEnv):
         self.write_video = write_video
         self.number_of_right_players_agent_controls = number_of_right_players_agent_controls
         self.seed = seed
+        self.sight_field = sight_field #视野范围，自定义
 
         self.env = football_env.create_environment(
             write_full_episode_dumps=self.write_full_episode_dumps,
@@ -72,6 +80,15 @@ class GoogleFootballEnv(MultiAgentEnv):
         self.n_actions = self.action_space[0].n
         self.obs = None
 
+        #针对qplex的obs_dim参数，但是目前不知道怎么得到这个参数，所以写死，疑似就是obs的一维长度。
+        self.obs_dim = None
+        if self.env_name == "academy_3_vs_1_with_keeper":
+            self.obs_dim = 26
+        if self.env_name == "academy_corner":
+            self.obs_dim = 34
+        if self.env_name == "academy_3_vs_1_with_keeper":
+            self.obs_dim = 34
+
     def step(self, _actions):
         """Returns reward, terminated, info."""
         if th.is_tensor(_actions):
@@ -82,6 +99,8 @@ class GoogleFootballEnv(MultiAgentEnv):
         obs, rewards, done, infos = self.env.step(actions.tolist())
 
         self.obs = obs
+
+        self.full_obs = self.env.unwrapped.observation()[0] #方便自定义参数
 
         if self.time_step >= self.episode_limit:
             done = True
@@ -146,3 +165,47 @@ class GoogleFootballEnv(MultiAgentEnv):
 
     def get_stats(self):
         return  {}
+
+    # __________________________________________________________________________
+    # DIFFER专用修改
+    # ___________________________________________________________________________
+    def get_indi_terminated(self):
+        #differ用的，获取个体存活标签。
+        terminate = []
+        for agent in range(self.n_agents):
+            if self.full_obs["left_team_yellow_card"][agent] == False:
+                terminate.append(0)
+            else:
+                terminate.append(1)
+        return terminate
+
+    # __________________________________________________________________________
+    # INSPIRE专用修改
+    # ___________________________________________________________________________
+    def get_ally_visibility_matrix(self):
+        #inspire用的，获取队友的可见矩阵，为[n_agent,n_agent]，可见为1，反之0
+        matrix = [[0 for _ in range(self.n_agents)] for _ in range(self.n_agents)]
+        for agent in range(self.n_agents):
+            pos_x,pos_y = self.full_obs["left_team_direction"][agent]
+            for ally_agent in range(agent, self.n_agents):
+                if agent == ally_agent:
+                    matrix[agent][ally_agent] = 1
+                else:
+                    ally_x,ally_y = self.full_obs["left_team_direction"][agent]
+                    if abs(pos_x - ally_x) + abs(pos_y - ally_y) <= self.sight_field:
+                        matrix[agent][ally_agent] = 0
+                        matrix[ally_agent][agent] = 0
+                    else:
+                        matrix[agent][ally_agent] = 1
+                        matrix[ally_agent][agent] = 1
+        list = []
+        for i in range(self.n_agents):
+            row = matrix[i]
+            binary_str = ''.join(map(str,row))
+            list.append(int(binary_str,2))
+        matrix = list
+        return matrix
+
+    # __________________________________________________________________________
+    # ICES专用修改
+    # ___________________________________________________________________________
